5453e51d36c8d460f863c9672263516c67b4e249
court-tek <dishoughng@gmail.com>
2018-11-11 00:38:21 -0800

    First commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main')
+
+@section('title')
+  Courtney Girley - Web Developer / Frontend Developer/ PHP
+@endsection
+
+
+@section('content')
+  <section id="home" class="row">
+      <div class="info col-md-6">
+        <div class="borderBox">
+          <h1 class="me">Courtney</h1>
+          <h1 class="meagain">Girley</h1>
+          <p>Frontend Web Developer</p>
+          <a href="">Check out my latest works</a>
+        </div>
+      </div>
+      <div class="logo">
+        cg.
+      </div>
+      <div class="message">
+        <div class="quotations">
+
+        </div>
+        <div class="quote">
+          The Crowd
+        </div>
+      </div>
+  </section>
+@endsection
