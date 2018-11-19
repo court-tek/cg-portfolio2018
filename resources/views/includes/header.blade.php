37b8497af2b03b1316943ebafd54f30483f2cb6d
court-tek <dishoughng@gmail.com>
2018-11-18 19:12:04 -0800

    changes made to header css

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,17 +7,15 @@
   <nav id="menu2" class="menu">
     {{-- Top --}}
     <div  class="menu-branding">
-      <div class="portrait">
-        hello
-      </div>
+      <dic src="" class="portrait"></div>
     </div>
     {{-- Top ends --}}
     {{-- Bottom --}}
     <ul id="other">
-      {{-- <li class="nav-item"><a href="{{ route('home') }}" class="nav-link current">Home</a></li>
+      <li class="nav-item"><a href="{{ route('home') }}" class="nav-link current">Home</a></li>
       <li class="nav-item"><a href="{{ route('skills') }}" class="nav-link">Skills</a></li>
       <li class="nav-item"><a href="{{ route('projects') }}" class="nav-link">Projects</a></li>
-      <li class="nav-item"><a href="{{ route('experience') }}" class="nav-link">Experience</a></li> --}}
+      <li class="nav-item"><a href="{{ route('experience') }}" class="nav-link">Experience</a></li>
     </ul>
     {{-- Bottom ends --}}
   </nav>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,31 +4,15 @@
   Courtney Girley - Web Developer / Frontend Developer/ PHP
 @endsection
 
-
 @section('content')
   <section id="home" class="row">
     <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
         <div class="content">
             <div class="title m-b-md">
-                Courtney Girley
+              Courtney Girley
             </div>
             <div id="test"></div>
             <div class="links">
-
             </div>
         </div>
     </div>
