05f404677d0b154cfcff22618f88cece1e61eaea
court-tek <dishoughng@gmail.com>
2018-11-30 13:41:40 -0800

    home page changed

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,7 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-              <span class="fname name">Courtney</span><span class="lname name">Girley</span>
+              <span class="fname name">You</span><span class="lname name">Girley</span>
             </div>
             <div id="test"></div>
             <div class="links">
