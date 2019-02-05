e6e44c2f4638e611d20268346141dfee8f29c5e0
court-tek <dishoughng@gmail.com>
2019-02-05 00:03:14 -0800

    added if to eventlistener

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -36,23 +36,25 @@ if (window.location.href === "http://localhost:8000/" || window.location.href ==
     console.log("no event listener");
 } else {
     let showMenu = false;
-    menuBtn.addEventListener("click", () => {
-        if (!showMenu) {
-            menuBtn.classList.add("openIt");
-            menu.classList.add("show");
-            menuBranding.classList.add("show");
-            navItems.forEach(item => item.classList.add("show"));
-            showMenu = true;
-            console.log(showMenu);
-        } else {
-            menuBtn.classList.remove("openIt");
-            menu.classList.remove("show");
-            menuBranding.classList.remove("show");
-            navItems.forEach(item => item.classList.remove("show"));
-            showMenu = false;
-            console.log(showMenu);
-        }
-    });
+    if (menuBtn) {
+      menuBtn.addEventListener("click", () => {
+          if (!showMenu) {
+              menuBtn.classList.add("openIt");
+              menu.classList.add("show");
+              menuBranding.classList.add("show");
+              navItems.forEach(item => item.classList.add("show"));
+              showMenu = true;
+              console.log(showMenu);
+          } else {
+              menuBtn.classList.remove("openIt");
+              menu.classList.remove("show");
+              menuBranding.classList.remove("show");
+              navItems.forEach(item => item.classList.remove("show"));
+              showMenu = false;
+              console.log(showMenu);
+          }
+      });
+    }
 }
 // hamburger menu ends
 
